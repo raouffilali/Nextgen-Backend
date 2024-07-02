0b69c57a896d2e25d57c3a38b1306f678e9592da
Raouf Filali <filaliabderaouf@yahoo.com>
2024-07-03 00:15:08 +0100

    final deployment to prod

---
+++ types/generated/components.d.ts
@@ -0,0 +1,56 @@
+import type { Struct, Schema } from '@strapi/strapi';
+
+export interface SkillsSkills extends Struct.ComponentSchema {
+  collectionName: 'components_skills_skills';
+  info: {
+    displayName: 'skills';
+  };
+  attributes: {
+    name: Schema.Attribute.String;
+    rating: Schema.Attribute.Integer;
+  };
+}
+
+export interface ExperienceExperience extends Struct.ComponentSchema {
+  collectionName: 'components_experience_experiences';
+  info: {
+    displayName: 'experience';
+    description: '';
+  };
+  attributes: {
+    title: Schema.Attribute.String;
+    companyName: Schema.Attribute.String;
+    city: Schema.Attribute.String;
+    state: Schema.Attribute.String;
+    startDate: Schema.Attribute.String;
+    endDate: Schema.Attribute.String;
+    workSummery: Schema.Attribute.RichText;
+    currentlyWorking: Schema.Attribute.Boolean;
+  };
+}
+
+export interface EducationEducation extends Struct.ComponentSchema {
+  collectionName: 'components_education_educations';
+  info: {
+    displayName: 'Education';
+    description: '';
+  };
+  attributes: {
+    universityName: Schema.Attribute.String;
+    degree: Schema.Attribute.String;
+    major: Schema.Attribute.String;
+    startDate: Schema.Attribute.String;
+    endDate: Schema.Attribute.String;
+    description: Schema.Attribute.Text;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'skills.skills': SkillsSkills;
+      'experience.experience': ExperienceExperience;
+      'education.education': EducationEducation;
+    }
+  }
+}
